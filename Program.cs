fbf0224869098cfcebd36bd3481f6ecb16ad5816
Christoper <c.blake@hotmail.se>
2021-06-15 10:39:20 +0200

    Three Hello Worlds

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace DebugGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
